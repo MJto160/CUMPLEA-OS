6ae0693f8e674dc80a83968fd3a5e85d770245e9
MJto160 <meliast2@miumg.edu.gt>
2026-07-27 15:33:00 -0600

    Fix: Corrigiendo nombre del paquete de Supabase

--- project/src/lib/supabase.ts
+++ project/src/lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-client';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInN1YiI6ImdjYnhpcHRoam13cnZza21vdnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwODg2ODEsImV4cCI6MjAzNzY2NDY4MX0.2zaZlydnyT1a_z-r0b5zFAnO59T-1w_Z7R9bZ5X_4_M";
