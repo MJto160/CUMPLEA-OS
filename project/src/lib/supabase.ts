c5c2dc1841c10bc9bfba9b308611292cc2938b9d
MJto160 <meliast2@miumg.edu.gt>
2026-07-27 22:37:29 -0600

    Base propia conectada

--- project/src/lib/supabase.ts
+++ project/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInN1YiI6ImdjYnhpcHRoam13cnZza21vdnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwODg2ODEsImV4cCI6MjAzNzY2NDY4MX0.2zaZlydnyT1a_z-r0b5zFAnO59T-1w_Z7R9bZ5X_4_M";
+const supabaseUrl = "https://kdmtmfxuzgfccqvzwjut.supabase.co";
+const supabaseAnonKey = "sb_publisable_0pG3gIGg3aVUc7HlpesuNA_Rf51te7DcoU6zZ27H29o";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Faltan las credenciales de Supabase.");
