803b2e942b92a4d71d1dbf570c2a2557175a394d
MJto160 <meliast2@miumg.edu.gt>
2026-07-27 23:17:31 -0600

    Configuración correcta de Supabase

--- project/src/lib/supabase.ts
+++ project/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://kdmtmfxuzgfccqvzwjut.supabase.co";
-const supabaseAnonKey = "sb_publisable_0pG3gIGg3aVUc7HlpesuNA_Rf51te7DcoU6zZ27H29o";
+const supabaseAnonKey = "sb_publishable_0pGJgIGg3aVUc7HlpesuNA_Rf51tes6";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Faltan las credenciales de Supabase.");
